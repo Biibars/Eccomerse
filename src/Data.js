090c7be15ed7f4721fc45bcbca08ab3479cd4565
Biibars <117353527+Biibars@users.noreply.github.com>
2023-05-25 07:47:59 +0600

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,58 @@
+let shopItemsData = [
+  {
+    id: "fone",
+    name: "adidas",
+    price: 45,
+    desc: "Cartoon Astronaut T-Shirts",
+    img: "/img/products/f1.jpg",
+  },
+  {
+    id: "ftwo",
+    name: "adidas",
+    price: 45,
+    desc: "Cartoon Astronaut T-Shirts",
+    img: "/img/products/f2.jpg",
+  },
+  {
+    id: "fthree",
+    name: "adidas",
+    price: 45,
+    desc: "Cartoon Astronaut T-Shirts",
+    img: "/img/products/f3.jpg",
+  },
+  {
+    id: "ffour",
+    name: "adidas",
+    price: 45,
+    desc: "Cartoon Astronaut T-Shirts",
+    img: "/img/products/f4.jpg",
+  },
+  {
+    id: "ffive",
+    name: "adidas",
+    price: 45,
+    desc: "Cartoon Astronaut T-Shirts",
+    img: "/img/products/f5.jpg",
+  },
+  {
+    id: "fsix",
+    name: "adidas",
+    price: 45,
+    desc: "Cartoon Astronaut T-Shirts",
+    img: "/img/products/f6.jpg",
+  },
+  {
+    id: "fseven",
+    name: "adidas",
+    price: 45,
+    desc: "Cartoon Astronaut T-Shirts",
+    img: "/img/products/f7.jpg",
+  },
+  {
+    id: "feiht",
+    name: "adidas",
+    price: 45,
+    desc: "Cartoon Astronaut T-Shirts",
+    img: "/img/products/f8.jpg",
+  },
+];
